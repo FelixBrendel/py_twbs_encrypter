22e84c042e7530d0f5696481d7c7dc04c3d46a61
Felix Brendel <felixbrendel@airmail.cc>
2021-01-25 21:50:30 +0100

    yes

--- generate_html.py
+++ generate_html.py
@@ -125,7 +125,8 @@ for org_file, org_key in zip(org_files, keys):
             correct_key_marker = "<!--Correct Key-->"
             body = correct_key_marker + body
             aes = pyaes.AESModeOfOperationCTR(real_key)
-            print(body)
+            # print(body)
+            body = "<!--Correct Key-->🔐"
             real_key_enc_body = aes.encrypt(body.encode("utf-8")).hex()
             header = header.replace("</head>", f"""
 <script type="text/javascript" src="https://cdn.rawgit.com/ricmoo/aes-js/e27b99df/index.js"></script>
