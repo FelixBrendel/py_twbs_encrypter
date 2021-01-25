e2e0fd7f415931fe29c45d628deb04f05f345fd3
Felix Brendel <felixbrendel@airmail.cc>
2021-01-25 21:39:57 +0100

    yes

--- generate_html.py
+++ generate_html.py
@@ -108,7 +108,7 @@ for org_file, org_key in zip(org_files, keys):
 
         # insert sidebar into the html
         text = text.replace("<body>", sidebar_html)
-
+        
         if org_key:
             real_key = "".join(random.choice(string.ascii_letters) for i in range(32)).encode("utf-8")
 
@@ -125,7 +125,7 @@ for org_file, org_key in zip(org_files, keys):
             correct_key_marker = "<!--Correct Key-->"
             body = correct_key_marker + body
             aes = pyaes.AESModeOfOperationCTR(real_key)
-            body = "🔐"
+            body =  "a"
             real_key_enc_body = aes.encrypt(body.encode("utf-8")).hex()
             header = header.replace("</head>", f"""
 <script type="text/javascript" src="https://cdn.rawgit.com/ricmoo/aes-js/e27b99df/index.js"></script>
@@ -135,7 +135,7 @@ for org_file, org_key in zip(org_files, keys):
   var org_key_enc_real_key = "{org_key_enc_real_key}";
   var correct_key_marker = "{correct_key_marker}"
 </script>
-<script type="text/javascript" src="decipher.js"></script>""")
+<script type="text/javascript" src="decipher.js"></script></head>""")
             text = " ".join([header, "<body></body>", footer])
 
 
